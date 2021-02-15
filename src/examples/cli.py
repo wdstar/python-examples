4cf1fe9ff1b5079f12be2ddb66ab6f19e4b3f5ef
wdstar <wdstar@users.noreply.github.com>
2021-02-15 20:06:08 +0900

    improve argparse setup.

--- src/examples/cli.py
+++ src/examples/cli.py
@@ -19,19 +19,24 @@ def pod_delete(args):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.set_defaults(cmd_help=parser.print_help)
-    subparsers = parser.add_subparsers(help="sub-command")
+    subparsers = parser.add_subparsers(dest="resource", help="target resource")
+    # Note: In advance `dest` key word arg. of add_subparsers() must be set.
+    subparsers.required = True
+    # Alternative: (Python 3.7 or later)
+    # subparsers = parser.add_subparsers(
+    #     dest="resource", required=True, help="target resource"
+    # )
 
     parser_node = subparsers.add_parser("node", help="manage nodes")
-    parser_node.set_defaults(cmd_help=parser_node.print_help)
-    subparser_node = parser_node.add_subparsers()
+    subparser_node = parser_node.add_subparsers(dest="verb", help="operation")
+    subparser_node.required = True
 
     parser_node_list = subparser_node.add_parser("list", help="list nodes.")
     parser_node_list.set_defaults(cmd_func=node_list)
 
     parser_pod = subparsers.add_parser("pod", help="manage pods")
-    parser_pod.set_defaults(cmd_help=parser_pod.print_help)
-    subparser_pod = parser_pod.add_subparsers()
+    subparser_pod = parser_pod.add_subparsers(dest="verb", help="operation")
+    subparser_pod.required = True
 
     parser_pod_list = subparser_pod.add_parser("list", help="list pods")
     parser_pod_list.set_defaults(cmd_func=pod_list)
