339bde4e1055a021f8d075f0645bd9f76545490a
wdstar <wdstar@users.noreply.github.com>
2021-02-21 20:10:02 +0900

    add retry settings for requests.

--- src/examples/requests_sample.py
+++ src/examples/requests_sample.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import requests
+from requests.sessions import session
+from urllib3.util import Retry
+from requests.adapters import HTTPAdapter
 
 proxies = {
     # "https": "http://proxy.example.com:3128/",
@@ -7,13 +10,30 @@ proxies = {
 }
 
 
+def get_session():
+    session = requests.Session()
+    retries = Retry(
+        total=5,
+        backoff_factor=1,
+        status_forcelist=[500, 502, 503, 504],
+    )
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    return session
+
+
 def main():
-    res = requests.get("https://www.google.com/", proxies=proxies)
+    url = "https://www.google.com/"
+    res = requests.get(url, proxies=proxies)
     print(res)
     print(res.url)
     print(res.headers)
     print(res.status_code)
     print(res.text)
+
+    session = get_session()
+    res = session.get(url, proxies=proxies)
+    res.raise_for_status()
+    print(res)
 
 
 if __name__ == "__main__":
